23f92737835d1007a0a06878e0462b144cf426ae
Fatih Emin Öge <fatiheminoge@gmail.com>
2020-11-17 22:39:33 +0300

    prediction method and guı will be added

--- train_caption.py
+++ train_caption.py
@@ -1,4 +1,5 @@
 import pickle
+
 import keras
 import numpy as np
 
@@ -36,16 +37,6 @@ def load_photo_features(filename,dataset):
     features = {k: all_features[k] for k in dataset}
     return features
 
-filename = 'Flicker8k_text/Flickr_8k.trainImages.txt'
-train = load_set(filename)
-print('Dataset: %d' % len(train))
-
-train_descriptions = load_clean_descriptions('descriptions.txt',train)
-print('Descriptions: train=%d' % len(train_descriptions))
-
-train_features = load_photo_features('features.pkl',train)
-print('Photos: train=%d' % len(train_features))
-
 def to_lines(descriptions):
     all_desc = list()
     for key in descriptions.keys():
@@ -62,18 +53,78 @@ def create_sequences(tokenizer,max_len,descriptions,photos,vocab_size):
     X1,X2,y = list(),list(),list()
     
     for key,desc_list in descriptions.items():
-        seq = tokenizer.text_to_sequences([desc])[0]
-        for i in range(1,len(seq)):
-            in_seq, out_seq = seq[:i],seq[i:]
-            in_seq = keras.preprocessing.sequence.pad_sequences([in_seq],maxlen=max_len)[0]
-            out_seq = keras.preprocessing.utils.to_categorical([out_seq],num_classes=vocab_size)[0]
+        for desc in desc_list:
+            seq = tokenizer.texts_to_sequences([desc])[0]
+            for i in range(1,len(seq)):
+                in_seq, out_seq = seq[:i],seq[i]
+                in_seq = keras.preprocessing.sequence.pad_sequences([in_seq],maxlen=max_len)[0]
+                out_seq = keras.utils.to_categorical([out_seq],num_classes=vocab_size)[0]
 
-            X1.append(photos[key][0])
-            X2.append(in_seq)
-            y.append(out_seq)
+                X1.append(photos[key][0])
+                X2.append(in_seq)
+                y.append(out_seq)
         return np.array(X1),np.array(X2),np.array(y)
 
 def max_length(descriptions):
     lines = to_lines(descriptions)
     return max(len(d.split()) for d in lines)
 
+def define_model(vocab_size,max_length):
+    inputs1 = keras.layers.Input(shape=(4096,))
+    fe1 = keras.layers.Dropout(0.5)(inputs1)
+    fe2 = keras.layers.Dense(256,activation='relu')(fe1)
+
+    inputs2 = keras.layers.Input(shape=(max_length,))
+    se1 = keras.layers.Embedding(vocab_size,256,mask_zero=True)(inputs2)
+    se2 = keras.layers.Dropout(0.5)(se1)
+    se3 = keras.layers.LSTM(256)(se2)
+    
+    decoder1 = keras.layers.add([fe2,se3])
+    decoder2 = keras.layers.Dense(256,activation='relu')(decoder1)
+    outputs = keras.layers.Dense(vocab_size,activation='softmax')(decoder2)
+
+    model = keras.Model(inputs=[inputs1,inputs2],outputs=outputs)
+    model.compile(loss='categorical_crossentropy',optimizer='adam')
+
+    print(model.summary())
+ #   keras.utils.plot_model(model, to_file='model.png',show_shapes=True)
+    return model
+
+filename = 'Flicker8k_text/Flickr_8k.trainImages.txt'
+train = load_set(filename)
+print('Dataset: %d' % len(train))
+
+train_descriptions = load_clean_descriptions('descriptions.txt',train)
+print('Descriptions: train=%d' % len(train_descriptions))
+
+train_features = load_photo_features('features.pkl',train)
+print('Photos: train=%d' % len(train_features))
+
+tokenizer = create_tokenizer(train_descriptions)
+vocab_size = len(tokenizer.word_index) + 1
+print('Vocabulary Size: %d' % vocab_size)
+
+max_length = max_length(train_descriptions)
+print('Max Length: %d' % max_length)
+
+X1train, X2train, ytrain = create_sequences(tokenizer, max_length, train_descriptions, train_features, vocab_size)
+
+filename = 'Flicker8k_text/Flickr_8k.devImages.txt'
+test = load_set(filename)
+print('Dataset: %d' % len(test))
+
+test_descriptions = load_clean_descriptions('descriptions.txt',test)
+print('Descriptions: test=%d' % len(train_descriptions))
+
+test_features = load_photo_features('features.pkl',test)
+print('Photos: test=%d' % len(test_features))
+
+X1test, X2test, ytest = create_sequences(tokenizer, max_length, test_descriptions, test_features, vocab_size)
+
+model = define_model(vocab_size,max_length)
+filepath = 'models/model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
+checkpoint = keras.callbacks.ModelCheckpoint(filepath,monitor='val_loss',verbose=1,save_best_only=True,mode='min')
+
+
+model.fit([X1train, X2train],ytrain, epochs=20, verbose=2, callbacks=[checkpoint], validation_data=([X1test,X2test],ytest))
+    
